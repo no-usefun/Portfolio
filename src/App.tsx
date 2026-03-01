a11f9c9986f7a247b71b9c2d1f55827357c469fc
Harsh Agarwal <itsharsh636@gmail.com>
2026-03-01 15:42:25 +0530

    Started building portfolio

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-500">Tailwind Working</h1>
+    </div>
+  );
+}
+
+export default App;
